618a4718faa17f3e29439f3b7df0a97c6e26142d
Akash-Dinkar-Bhujbal <akashbhujbal97@gmail.com>
2022-04-02 20:49:46 +0530

    Second Commit

--- User_Module/src/main/java/com/cg/smms/entity/User.java
+++ User_Module/src/main/java/com/cg/smms/entity/User.java
@@ -12,16 +12,18 @@ public class User {
 	private Integer id;
 	private String name;
 	private String password;
+	private String type;
 
 	public User() {
 
 	}
 
-	public User(Integer id, String name, String password) {
+	public User(Integer id, String name, String password, String type) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.password = password;
+		this.type = type;
 	}
 
 	@Id
@@ -50,9 +52,17 @@ public class User {
 		this.password = password;
 	}
 
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", password=" + password + "]";
+		return "User [id=" + id + ", name=" + name + ", password=" + password + ", type=" + type + "]";
 	}
 
 }
